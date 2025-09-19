a6b805b412373cefbea5fb4d3c175ff552e60b1f
Ahmadov Avazagha <ahmadov.avazagha@gmail.com>
2025-09-19 22:18:51 +0200

    added a small helper to time the functions

--- src/stock_solver/dataset/utils.py
+++ src/stock_solver/dataset/utils.py
@@ -5,6 +5,17 @@ from dotenv import load_dotenv
 import src.stock_solver.dataset.apis.alpha_vantage as AV
 import pandas as pd
 
+from contextlib import contextmanager
+import time
+
+@contextmanager
+def tmark(label: str):
+    t0 = time.perf_counter()
+    yield
+    dt = time.perf_counter() - t0
+    print(f"time [{label}]: {dt:.4f}s")
+
+
 class API_KEYS(NamedTuple):
     alpaca_api: str
     alpaca_secret: str
@@ -26,26 +37,31 @@ def api_keys() -> API_KEYS:
 
 
 def fetch_daily_OHLCV(symbol: str) -> pd.DataFrame:
-    response = AV.TimeSeriesDailyRequest(symbol=symbol).query()
-    result = AV.TimeSeriesResult.parse(response.json())
+    with tmark("request"):
+        response = AV.TimeSeriesDailyRequest(symbol=symbol).query()
+    with tmark("parsing"):
+        result = AV.TimeSeriesResult.parse(response.json())
 
-    raw = {ts_str: ohlcv.model_dump() for ts_str, ohlcv in result.time_series.items()}
-    if not raw:
-        print(f"Daily Time Series for {symbol} are missing, skipping.")        
-        raise AV.APIError()
-    
-    df = pd.DataFrame.from_dict(raw, orient="index")
-    idx = pd.to_datetime(df.index, errors="coerce")
-    df.index = idx
-    df.index.name = "date"
-    df = df.sort_index()
+    with tmark("build_dataframe"):
+        raw = {ts_str: ohlcv.model_dump() for ts_str, ohlcv in result.time_series.items()}
+        if not raw:
+            print(f"Daily Time Series for {symbol} are missing, skipping.")        
+            raise AV.APIError()
+        
+        df = pd.DataFrame.from_dict(raw, orient="index")
+
+    with tmark("indexing"):
+        idx = pd.to_datetime(df.index, errors="coerce")
+        df.index = idx
+        df.index.name = "date"
+        df = df.sort_index()
 
     for col in ("open", "high", "low", "close", "adjusted_close"):
         df[col] = pd.to_numeric(df[col], errors='coerce').astype('float32')
     df["volume"] = pd.to_numeric(df["volume"], errors='coerce').astype('int64')
 
-    print(df.info())
-    print(df)
+    # print(df.info())
+    # print(df)
     return df
 
 
@@ -66,5 +82,5 @@ if __name__ == '__main__':
 
     
     print(f"avg #rows = {sum(len(fetch_daily_OHLCV(ticker).index) for ticker in tickers) / len(tickers)}")
-    for ticker in tickers:
-        print(f'start day for {ticker} is {min(fetch_daily_OHLCV(ticker).index)}') 
+    # for ticker in tickers:
+    #     print(f'start day for {ticker} is {min(fetch_daily_OHLCV(ticker).index)}') 
