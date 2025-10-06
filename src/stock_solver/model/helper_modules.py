a8b7af072740b7b6d48a60d21b2d029e5ccdf20d
Ahmadov Avazagha <ahmadov.avazagha@gmail.com>
2025-10-07 01:14:47 +0200

    Added token/value embedding

--- src/stock_solver/model/helper_modules.py
+++ src/stock_solver/model/helper_modules.py
@@ -51,3 +51,19 @@ class TickerEmbedding(torch.nn.Module):
         embedding = self.embedding(x.view(-1))
         embedding = embedding.unsqueeze(1).expand(-1, length, -1) # [B, L, D]
         return self.drop(embedding)
+
+class ValueEmbedding(torch.nn.Module):
+    def __init__(self, dim_in: int, dim_out: int):
+        super().__init__() # type: ignore
+        self.layer = torch.nn.Conv1d(
+            in_channels=dim_in,
+            out_channels=dim_out,
+            kernel_size=3,
+            padding=1,
+            padding_mode='circular'
+        )
+        torch.nn.init.kaiming_uniform_(self.layer.weight)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # [B, L, in] -> [B, in, L] -> conv -> [B, out, L] -> [B, L, out]
+        return self.layer(x.transpose(1, 2)).transpose(1, 2)
