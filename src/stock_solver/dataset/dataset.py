95384a20d8ce47122167e2865674a230b1f4f197
Ahmadov Avazagha <ahmadov.avazagha@gmail.com>
2025-10-13 16:56:45 +0200

    added type alias, added test instances

--- src/stock_solver/dataset/dataset.py
+++ src/stock_solver/dataset/dataset.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as np
 
 from dataclasses import dataclass
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, TypeAlias
 from .apis.alpha_vantage_calls import populate_dataset
+
+TrainElement: TypeAlias = Tuple[Tuple[torch.Tensor, torch.Tensor], Tuple[torch.Tensor, torch.Tensor], int]
+TestElement: TypeAlias = Tuple[Tuple[torch.Tensor, torch.Tensor], torch.Tensor, int]
+Element: TypeAlias = TrainElement | TestElement
 
 @dataclass
 class WindowIndex:
@@ -12,12 +16,13 @@ class WindowIndex:
     start: int
 
 
-class MultiTickerDataset(torch.utils.data.Dataset[Tuple[torch.Tensor, torch.Tensor, int]]):
-    feature_cols: List[str] = ["open", "high", "low", "adjusted_close",]
+class MultiTickerDataset(torch.utils.data.Dataset[Element]):
+    feature_cols: List[str] = ["open", "high", "low", "adjusted_close", "news_sent_wsum", "insider_transaction"]
     target_col: str = "close"
 
-    def __init__(self, data: Dict[str, pd.DataFrame], lookback: int, horizon: int):
+    def __init__(self, data: Dict[str, pd.DataFrame], lookback: int, horizon: int, is_test: bool = False):
         super().__init__()
+        self.is_test = is_test
 
         self.data = data
         self.L = lookback
@@ -27,13 +32,15 @@ class MultiTickerDataset(torch.utils.data.Dataset[Tuple[torch.Tensor, torch.Tens
         self.dates: List[pd.DatetimeIndex] = []
         self.tickers = list(data.keys())
         self.win: List[WindowIndex] = []
+
         for ticker_id, ticker in enumerate(self.tickers):
             df = data[ticker]
-            x = df[MultiTickerDataset.feature_cols].to_numpy(dtype=np.float32)
-            y = df[MultiTickerDataset.target_col].to_numpy(dtype=np.float32)
-            self.X.append(x)
-            self.Y.append(y)
             self.dates.append(pd.DatetimeIndex(df.index))
+            x = df[MultiTickerDataset.feature_cols].to_numpy(dtype=np.float32)
+            self.X.append(x)
+            if not is_test:
+                y = df[MultiTickerDataset.target_col].to_numpy(dtype=np.float32)
+                self.Y.append(y)
             for date_index in range(self.L, len(df) - self.H):
                 self.win.append(WindowIndex(ticker_id=ticker_id, start=date_index - self.L))
 
@@ -48,24 +55,23 @@ class MultiTickerDataset(torch.utils.data.Dataset[Tuple[torch.Tensor, torch.Tens
     def __len__(self) -> int:
         return len(self.win)
 
-    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor, int, torch.Tensor, torch.Tensor]:
+    def __getitem__(self, idx: int) -> Element:
         # TODO: add normalization here
         window = self.win[idx]
         ticker_id, start = window.ticker_id, window.start
-
         x = self.X[ticker_id][start: start + self.L]
-        y = self.Y[ticker_id][start + self.L: start + self.L + self.H] 
-        enc_dates = self.dates[ticker_id][start: start + self.L]
-        dec_dates = self.dates[ticker_id][start + self.L: start + self.L + self.H]
-
         x = torch.from_numpy(x)
-        y = torch.from_numpy(y)
-        enc_marks = MultiTickerDataset._date_mark(enc_dates)
-        dec_marks = MultiTickerDataset._date_mark(dec_dates)
-        return x, y, ticker_id, enc_marks, dec_marks
+        enc_marks = MultiTickerDataset._date_mark(self.dates[ticker_id][start: start + self.L])
+        dec_marks = MultiTickerDataset._date_mark(self.dates[ticker_id][start + self.L: start + self.L + self.H])
+        if not self.is_test:
+            y = self.Y[ticker_id][start + self.L: start + self.L + self.H]
+            y = torch.from_numpy(y) 
+            return (x, enc_marks), (y, dec_marks), ticker_id
+        return (x, enc_marks), dec_marks, ticker_id
 
 
-def collate(batch):
+
+def collate(batch: List[Element]):
     xs, ys, ticker_ids, enc_marks, dec_marks = batch
     x = torch.stack(xs, dim=0)
     y = torch.stack(ys, dim=0)
@@ -79,10 +85,10 @@ if __name__ == '__main__':
     tickers = [
     "AAPL",
     "MSFT",
-    # "AMZN",
-    # "GOOG",
-    # "GOOGL",
-    # "META",
+    "AMZN",
+    "GOOG",
+    "GOOGL",
+    "META",
     ]
 
     data = populate_dataset(tickers)
