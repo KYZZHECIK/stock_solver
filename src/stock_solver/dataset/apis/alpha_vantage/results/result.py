16f4f19ae9fdb4c9e04a6d3ee379e6c2da5646ab
Ahmadov Avazagha <ahmadov.avazagha@gmail.com>
2025-10-13 16:54:38 +0200

    added a small TODO for future features

--- src/stock_solver/dataset/apis/alpha_vantage/results/result.py
+++ src/stock_solver/dataset/apis/alpha_vantage/results/result.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Any, Self
 
 class Result(BaseModel):
+    # TODO: remove parse and _is_invalid_date, use Pydantic validators and parsers 
     @classmethod
     def parse(cls, data: dict[str, Any]) -> Self:
         if cls._is_invalid_data(data):
